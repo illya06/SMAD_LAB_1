bb4e3e7c605dbfe6623e7b0dddb4c402cf6ec77d
illya06 <47461573+illya06@users.noreply.github.com>
2021-02-17 10:21:31 +0200

    Added some data calculations

--- js/work.js
+++ js/work.js
@@ -1,2 +1,43 @@
 let numbers = [0.36, 0.38, 0.38, 0.37, 0.40, 0.38, 0.36];
 
+
+function main(){
+    
+    // mid Statistical
+    let midStatistical = numbers.reduce((a, b) => a + b, 0)/numbers.length;
+    document.getElementById('midStatistical').innerHTML = ` <kbd>${midStatistical}</kbd>`;
+
+    //trend
+    let trend = numbers.reduce(function(a, b) {
+        return Math.max(a, b);
+    });
+    document.getElementById('trend').innerHTML = ` <kbd>${trend}</kbd>`;
+
+    //median
+    // let temp = numbers;
+    // temp.sort(function(a,b){
+    //     return a-b;
+    //   });
+    
+    //   var half = Math.floor(values.length / 2);
+    
+    //   if (values.length % 2)
+    //     return values[half];
+    
+    //   return (values[half - 1] + values[half]) / 2.0;
+
+    //span
+    let max = trend;
+    let min = numbers.reduce(function(a, b) {
+        return Math.min(a, b);
+    });
+    document.getElementById('span').innerHTML = ` <kbd>${min} - ${max}</kbd>`;
+
+    //dispersion
+    null;
+    
+    //standard dispersion
+    null;
+
+    
+}
