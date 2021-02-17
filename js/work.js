13d8381dd0788ac5ef3995d75fb4828a95408aa2
illya06 <47461573+illya06@users.noreply.github.com>
2021-02-17 11:49:32 +0200

    Added tetst switch

--- js/work.js
+++ js/work.js
@@ -1,20 +1,25 @@
-let numbers = [0.36, 0.38, 0.38, 0.37, 0.40, 0.38, 0.36];
+const test = true;
 
-let numFrequency = new Map([
-    [0.36, 2],
-    [0.37, 1],
-    [0.38, 3],
-    [0.40, 1]
-]);
+if (test) {
+    let numbers = [0.36, 0.38, 0.38, 0.37, 0.40, 0.38, 0.36];
 
-function main(){
-    
+    let numFrequency = new Map([
+        [0.36, 2],
+        [0.37, 1],
+        [0.38, 3],
+        [0.40, 1]
+    ]);
+}
+
+
+function main() {
+
     // mid Statistical
-    let midStatistical = numbers.reduce((a, b) => a + b, 0)/numbers.length;
+    let midStatistical = numbers.reduce((a, b) => a + b, 0) / numbers.length;
     document.getElementById('midStatistical').innerHTML = ` <kbd>${midStatistical}</kbd>`;
 
     //trend
-    let trend = numbers.reduce(function(a, b) {
+    let trend = numbers.reduce(function (a, b) {
         return Math.max(a, b);
     });
     document.getElementById('trend').innerHTML = ` <kbd>${trend}</kbd>`;
@@ -24,26 +29,26 @@ function main(){
     // temp.sort(function(a,b){
     //     return a-b;
     //   });
-    
+
     //   var half = Math.floor(values.length / 2);
-    
+
     //   if (values.length % 2)
     //     return values[half];
-    
+
     //   return (values[half - 1] + values[half]) / 2.0;
 
     //span
     let max = trend;
-    let min = numbers.reduce(function(a, b) {
+    let min = numbers.reduce(function (a, b) {
         return Math.min(a, b);
     });
     document.getElementById('span').innerHTML = ` <kbd>${min} - ${max}</kbd>`;
 
     //dispersion
     null;
-    
+
     //standard dispersion
     null;
 
-    
+
 }
